db71b17a528b4a025525613bdbeca9defc2ec6f1
aryan21617 <aryanpurbey1617@gmail.com>
2025-10-04 07:01:19 +0530

    Added Shimmer UI to our project

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,25 +1,30 @@
 import RestaurantCard from "./RestaurantCard";
 import { resList } from "../utils/mockData";
 import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  const [listOfRestaurants, setListOfRestaurants] = useState(resList || []);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
 
-  const fetchData = async () => {
-    const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
-    );
-    const json = await data.json();
-    console.log(json);
+  // const fetchData = async () => {
+  //   const data = await fetch(
+  //     "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+  //   );
+  //   const json = await data.json();
+  //   console.log(json);
 
-    setListOfRestaurants(
-      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
-    );
-  };
+  //   setListOfRestaurants(
+  //     json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+  //   );
+  // };
+
+  if(listOfRestaurants.length === 0 ) {
+    return <Shimmer />;
+  }
 
   return (
     <div className="body">
