28fc95fe000998d2068f79ad9dc8c238d36ef39b
aryan21617 <aryanpurbey1617@gmail.com>
2025-10-06 11:39:14 +0530

    Added Error  page using Router

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Footer } from "./components/Footer";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import Error from "./components/Error";
 
 const AppLayout = () => {
   return (
@@ -21,6 +22,7 @@ const appRouter = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
+    errorElement: <Error />,
   },
   {
     path: "/about",
