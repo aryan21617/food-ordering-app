3f8aefb071e9b74dba8b89424d071de8772c0c2d
aryan21617 <aryanpurbey1617@gmail.com>
2025-10-07 06:49:11 +0530

    Used useRouteError  hook

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,12 @@
+import { useRouteError } from "react-router-dom";
+
 const Error  = () => {
+  const err = useRouteError();
+  //console.log(err);
   return (
     <div>
       <h1>404 - Page Not Found</h1>
+      <h2>{err.status}: {err.statusText}</h2>
     </div>
   );
 }
