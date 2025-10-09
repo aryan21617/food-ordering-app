88fc306cd8e6eccf80c9de603d602850b68db89a
aryan21617 <aryanpurbey1617@gmail.com>
2025-10-09 21:19:59 +0530

    updated shimmer UI

--- src/components/Shimmer.jsx
+++ src/components/Shimmer.jsx
@@ -1,24 +1,17 @@
 const Shimmer = () => {
   return (
     <div className="shimmer-container">
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
+      {Array.from({ length: 18 }, (_, index) => (
+        <div key={index} className="shimmer-card">
+          <div className="shimmer-image"></div>
+          <div className="shimmer-title"></div>
+          <div className="shimmer-cuisines"></div>
+          <div className="shimmer-rating"></div>
+          <div className="shimmer-time"></div>
+          <div className="shimmer-cost"></div>
+          <div className="shimmer-discount"></div>
+        </div>
+      ))}
     </div>
   );
 };
