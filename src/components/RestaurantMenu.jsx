7fecdb680f5e32635d7cb54f626e74b463865ead
aryan21617 <aryanpurbey1617@gmail.com>
2025-10-10 22:11:21 +0530

    Added Restarurant Menu page

---
+++ src/components/RestaurantMenu.jsx
@@ -0,0 +1,20 @@
+
+
+const RestaurantMenu = () => {
+  return (
+    <div className="menu">
+      <h1>Name of Restaurant</h1>
+      <h2>Menu</h2>
+      <ul>
+        <li>Paneer Butter Masala</li>
+        <li>Dal Makhani</li>
+        <li>Jeera Rice</li>
+        <li>Naan</li>
+        <li>Roti</li>
+        <li>Gulab Jamun</li>
+      </ul>
+    </div>
+  )
+}
+
+export default RestaurantMenu
